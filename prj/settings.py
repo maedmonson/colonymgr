7b0bb9554c14861dd723d91ac25efef5187d39d6
Mike Edmonson <maedmonson@gmail.com>
2018-12-18 09:15:12 -0600

    Add requirements.txt file

--- prj/settings.py
+++ prj/settings.py
@@ -142,3 +142,13 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 LOGIN_URL = 'login'
 
+
+from decouple import config, Csv
+import dj_database_url
+
+SECRET_KEY = config('SECRET_KEY')
+DEBUG = config('DEBUG', default=False, cast=bool)
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
+DATABASES = {
+    'default' : dj_database_url.config(default=config('DATABASE_URL'))
+}
